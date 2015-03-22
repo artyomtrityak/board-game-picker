5ef3628812c0e1761a79847bfb1b8dfb771befcf
Artyom Trityak <art.trityak@gmail.com>
2015-03-22 16:02:37 +0200

    Basic routing

--- app/javascript/routes.jsx
+++ app/javascript/routes.jsx
@@ -2,5 +2,15 @@ import React from 'react'
 import {Route, DefaultRoute} from 'react-router';
 import Router from 'react-router';
 
-console.log('it wo2', React, Route, Router);
+// Components
+import NoopComponent from './components/noop'
 
+var routes = (
+  <Route name='app' path='/' handler={NoopComponent}>
+    
+  </Route>
+);
+
+Router.run(routes, (Handler) => {
+  React.render(<Handler/>, document.getElementById('react-root'));
+});
